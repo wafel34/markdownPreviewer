8d3c70d54ce5182556d2391a6f0f3268035646d3
Pawel Marcinkowski <pmarcinkowski1016@timeinc.com>
2017-08-03 14:04:25 +0100

    Added component, render method, typing method and base css styling

--- components/scripts/app.js
+++ components/scripts/app.js
@@ -3,14 +3,27 @@ var React = require("react"),
     ReactDOM = require("react-dom");
 
 class MyComponenet extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          text: "Random text for a beginning"
+        };
+        this.typing = this.typing.bind(this);
+    }
+    typing (e){
+        var currentVal = e.target.value;
+        this.setState({
+            text: currentVal
+        });
+    }
     render() {
         return (
-            <div>Blaskowo1
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
+            <div className="container">
+                <textarea className="input-area" defaultValue={this.state.text}
+                onChange={this.typing}>
+
+                </textarea>
+                <div className="output-area">{this.state.text}</div>
             </div>
         );
     }
